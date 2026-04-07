a722454b2bf111960c081305c3f37d808314bcf7
Honghua Zhu <zhh200910@gmail.com>
2026-04-08 01:30:19 +0800

    删除init-data.sql

--- examples/petstore/run.bat
+++ examples/petstore/run.bat
@@ -2,7 +2,7 @@
 
 set JAVA_OPTS=-database petstore^
  -config sql/lealone.sql^
- sql/llm.sql sql/tables.sql sql/init-data.sql sql/services.sql
+ sql/llm.sql sql/tables.sql sql/services.sql
 
 java -jar %LEALONE_HOME%/lealone-8.0.0-SNAPSHOT.jar %JAVA_OPTS%
 
