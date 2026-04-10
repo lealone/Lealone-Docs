49bbc9ef08a5bee8eb8e5f0ad2f276635d191b0b
Honghua Zhu <zhh200910@gmail.com>
2026-04-10 16:37:05 +0800

    update services.sql

--- examples/petstore/sql/services.sql
+++ examples/petstore/sql/services.sql
@@ -6,11 +6,11 @@ create service if not exists user_service (
     login(user_id varchar, password varchar) varchar,
     logout(user_id varchar) varchar,
     register(user_id varchar, password varchar, password2 varchar) void,
-    update(account Account) void,
-    get_user(user_id varchar) varchar
+    update(account Account) void comment '如果account不存在就新增',
+    get_user(user_id varchar) varchar comment '把account信息也返回'
 );
 
-create service if not exists store_service (
+create service if not exists store_service comment '实现方法时要返回关联表的信息'(
     add_product(product Product) varchar,
     get_all_categories() varchar,
     get_all_product_items(product_id varchar) varchar
